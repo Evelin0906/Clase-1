0938d8b1885489272278e02b1af5421eab5f7b3b
Evelin0906 <78057770+Evelin0906@users.noreply.github.com>
2021-01-26 22:04:47 -0500

    Primera clase de pyhton

---
+++ main.py
@@ -0,0 +1,46 @@
+print("Hola mundo")
+
+# Input()
+print("Ingrese su nombre")
+x = input()
+print("Hola " + x)
+
+## esta variable...
+## para ayudar a otros
+
+print("Imprimir este texto")
+x = 40
+mensaje = "Hola a todos!"
+print(mensaje)
+print(x)
+
+# errores
+# sintaxError
+# NameError
+un_integer = 2
+un_float = 2.1
+
+print(un_integer+3)
+
+print(25*2)
+print(10/5)
+precio_cafe = 1.50
+numero_cafes = 4
+print(precio_cafe*numero_cafes)
+print(15 ** 2)
+print(4 ** 0.5)
+print(32 % 3)
+cumple_uno = "Yo tengo "
+Edad = 36
+Cumple_dos = " años"
+mensaje_completo = cumple_uno + str(Edad) + Cumple_dos
+print(mensaje_completo)
+
+numero_km = 5
+numero_km +=4
+print(numero_km)
+recorrido = "estuve recorriendo xxxx"
+recorrido += "running"
+recorrido += ", #5k"
+print(recorrido)
+###strings multilinea
